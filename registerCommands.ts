0b96bb14b17278d71659b8f561786054671c1f16
Ed Catboi Putans <edputans@gmail.com>
2022-09-06 16:01:12 +0100

    Event now uses epoch

--- registerCommands.ts
+++ registerCommands.ts
@@ -1,42 +1,72 @@
-import { SlashCommandBuilder, Routes, REST, SlashCommandStringOption } from 'discord.js';
-import { envVariables } from './utils/getEnvVariables';
-import { HOXCommand } from './utils/types';
+import {
+  SlashCommandBuilder,
+  Routes,
+  REST,
+  SlashCommandStringOption,
+} from "discord.js";
+import { envVariables } from "./utils/getEnvVariables";
+import { HOXCommand } from "./utils/types";
 
 const commands = [
-  new SlashCommandBuilder().setName(HOXCommand.solved).setDescription('Marks thread as solved'),
-  new SlashCommandBuilder().setName(HOXCommand.standup).setDescription('Post a standup message'),
-  new SlashCommandBuilder().setName(HOXCommand.unsolve).setDescription('Marked as solved by accident? No worries Im here for ya'),
+  new SlashCommandBuilder()
+    .setName(HOXCommand.solved)
+    .setDescription("Marks thread as solved"),
+  new SlashCommandBuilder()
+    .setName(HOXCommand.standup)
+    .setDescription("Post a standup message"),
+  new SlashCommandBuilder()
+    .setName(HOXCommand.unsolve)
+    .setDescription("Marked as solved by accident? No worries Im here for ya"),
 
-  // still an experimental feature. No API support.
-  // new SlashCommandBuilder().setName(HOXCommand.dangerous__clear_voice_channel).setDescription('Clear voice channel chat'),
-  new SlashCommandBuilder().setName(HOXCommand.zoom).addStringOption((option: SlashCommandStringOption) =>
-    option.setName('zoom-link')
-      .setDescription('Custom zoom link')
-      .setRequired(false))
+  new SlashCommandBuilder()
+    .setName(HOXCommand.zoom)
     .addStringOption((option: SlashCommandStringOption) =>
-      option.setName('message')
-        .setDescription('Set a custom message')
-        .setRequired(false))
-    .setDescription('Post zoom link!'),
+      option
+        .setName("zoom-link")
+        .setDescription("Custom zoom link")
+        .setRequired(false)
+    )
+    .addStringOption((option: SlashCommandStringOption) =>
+      option
+        .setName("message")
+        .setDescription("Set a custom message")
+        .setRequired(false)
+    )
+    .setDescription("Post zoom link!"),
 
-  new SlashCommandBuilder().setName(HOXCommand.event).addStringOption((option: SlashCommandStringOption) =>
-    option.setName('when')
-      .setDescription('when is the event taking place>')
-      .setRequired(true))
+  new SlashCommandBuilder()
+    .setName(HOXCommand.event)
     .addStringOption((option: SlashCommandStringOption) =>
-      option.setName('what')
+      option
+        .setName("when")
+        .setDescription("when is the event taking place>")
+        .setRequired(true)
+    )
+    .addStringOption((option: SlashCommandStringOption) =>
+      option
+        .setName("what")
         .setDescription("What's going on?")
-        .setRequired(true))
+        .setRequired(true)
+    )
     .addStringOption((option: SlashCommandStringOption) =>
-      option.setName('where')
+      option
+        .setName("where")
         .setDescription("Where is the event happening (Zoom, channel, IRL etc)")
-        .setRequired(true))
-    .setDescription('Create a new event in this channel'),
-]
-  .map(command => command.toJSON());
+        .setRequired(true)
+    )
 
-const rest = new REST({ version: '10' }).setToken(envVariables.token);
+    .setDescription("Create a new event in this channel"),
+].map((command) => command.toJSON());
 
-rest.put(Routes.applicationGuildCommands(envVariables.clientId, envVariables.guildId), { body: commands })
-  .then(() => console.log('Successfully registered application commands.'))
-  .catch(console.error); 
+const rest = new REST({ version: "10" }).setToken(envVariables.token);
+
+rest
+  .put(
+    Routes.applicationGuildCommands(
+      envVariables.clientId,
+      envVariables.guildId
+    ),
+    { body: commands }
+  )
+  .then(() => console.log("Successfully registered application commands."))
+  .catch(console.error);
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,34 +1,37 @@
 import { Interaction } from "discord.js";
-import { supportChannelIds, classRoomIds, zoomLinks } from "./consts";
+import { supportChannelIds, classRoomIds, staffConfig } from "./consts";
+import { UserID } from "./types";
 
 export const getIsSupportThread = (channelId: string) => {
   const isSupportThread = supportChannelIds.includes(channelId);
   return isSupportThread;
-}
+};
 export const getIsClassroomThread = (channelId: string) => {
   return classRoomIds.includes(channelId);
-}
+};
 
-export const createEphemeral = async (interaction: Interaction, content: string) => {
+export const createEphemeral = async (
+  interaction: Interaction,
+  content: string
+) => {
   if (!interaction.isCommand()) return;
   await interaction.reply({
     content,
-    ephemeral: true
-  })
+    ephemeral: true,
+  });
   return;
-}
+};
 
 // TODO: transform it to use interaction / message -> user -> roles instead
-export const getIsTeacher = (userId: string | number) => {
-  const userIdString = `${userId}`
-  return Object.keys(zoomLinks).includes(userIdString)
-}
+export const getIsTeacher = (userId: string) => {
+  return !!staffConfig[userId as UserID];
+};
 
 export const getIsStudent = (roleIds: string[]) => {
   const studentRoleIds = [
-    '955781997857480764', // ed's server - Plebian role
-    '866609200015867914' // Hoxton - student role
+    "955781997857480764", // ed's server - Plebian role
+    "866609200015867914", // Hoxton - student role
   ];
 
-  return !!roleIds.find(roleId => studentRoleIds.includes(`${roleId}`))
-}
+  return !!roleIds.find((roleId) => studentRoleIds.includes(`${roleId}`));
+};
